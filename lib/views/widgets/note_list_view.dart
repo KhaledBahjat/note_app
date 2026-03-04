9d709ae576a087364c34130df4763a2134818a73
KhaledBahjat <khaledbahjat0@gmail.com>
2026-03-04 11:49:35 +0200

    refactor: update NotesListView to assign dynamic colors to NoteItem for enhanced visual variety

--- lib/views/widgets/note_list_view.dart
+++ lib/views/widgets/note_list_view.dart
@@ -5,11 +5,18 @@ class NotesListView extends StatelessWidget {
   const NotesListView({
     super.key,
   });
-
+  final colors =const [
+    Color(0xfffecd7e),
+    Color(0xfff9c1c1),
+    Color(0xffb5eaea),
+    Color(0xfff6eac2),
+    Color(0xffd4a5a5),
+    Color(0xffc1fba4),
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemBuilder: (context, index) => const NoteItem(),
+      itemBuilder: (context, index) =>  NoteItem(color: colors[index % colors.length],),
       itemCount: 10,
       shrinkWrap: true,
       // physics: NeverScrollableScrollPhysics(),
