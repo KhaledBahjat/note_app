ff2786b4fa251621c364c95517caf00f19260c9e
KhaledBahjat <khaledbahjat0@gmail.com>
2026-03-04 11:29:52 +0200

    feat: implement NoteItem widget for displaying individual notes

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 20.h,bottom: 24.w,left: 16.w),
+      decoration: BoxDecoration(
+        color: Color(0xfffecd7e),
+        borderRadius: BorderRadius.circular(16.r),
+      ),
+
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Note Title',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28.sp,
+              ),
+            ),
+
+            subtitle: Padding(
+              padding:  EdgeInsets.symmetric(vertical: 16.h),
+              child: Text(
+                'Note content goes here...',
+                style: TextStyle(
+                  color: Colors.black.withValues(alpha: 0.5),
+                  fontSize: 18.sp,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.delete,
+                color: Colors.black,
+                size: 30.sp,
+              ),
+            ),
+          ),
+          Padding(
+            padding:  EdgeInsets.only(right: 16.w),
+            child: Text(
+              'Created on: 2024-06-01',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16.sp,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
