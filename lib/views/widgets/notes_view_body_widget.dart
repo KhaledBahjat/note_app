5f80d53e4b0aeab32c13d8159ab730b4f2c7df13
KhaledBahjat <khaledbahjat0@gmail.com>
2026-03-04 11:07:46 +0200

    feat: create NotesViewBody widget for structured note display

---
+++ lib/views/widgets/notes_view_body_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:note_app/helper/spacing.dart';
+import 'package:note_app/views/widgets/custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 20.w),
+      child: Column(
+        children: [
+          HeightSpace(height: 20),
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
