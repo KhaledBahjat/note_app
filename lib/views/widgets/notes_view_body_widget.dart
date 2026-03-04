ff8e73006e1481c9b07269f64e2c695cfec277cb
KhaledBahjat <khaledbahjat0@gmail.com>
2026-03-04 11:41:54 +0200

    refactor: replace NoteItem with NotesListView in NotesViewBody for improved note display

--- lib/views/widgets/notes_view_body_widget.dart
+++ lib/views/widgets/notes_view_body_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:note_app/helper/spacing.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
-import 'package:note_app/views/widgets/custom_note_item.dart';
+import 'package:note_app/views/widgets/note_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -16,11 +16,12 @@ class NotesViewBody extends StatelessWidget {
           HeightSpace(height: 20),
           CustomAppBar(),
           HeightSpace(height: 20),
-          NoteItem(),
+          Expanded(
+            child: NotesListView(),
+          ),
         ],
       ),
     );
   }
 }
-
 
