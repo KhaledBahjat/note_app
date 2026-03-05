826f747af3c4055be92242cc6050397b69facbc6
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-03-05 11:59:17 +0200

    feat: enhance NoteItem widget with gesture detection for editing notes

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,61 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key, required this.color});
   final Color color;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 8.h),
-      padding: EdgeInsets.only(top: 20.h,bottom: 24.w,left: 16.w),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(16.r),
-      ),
-
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Note Title',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 28.sp,
-              ),
-            ),
-
-            subtitle: Padding(
-              padding:  EdgeInsets.symmetric(vertical: 16.h),
-              child: Text(
-                'Note content goes here...',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => EditNoteView()));
+      },
+      child: Container(
+        margin: EdgeInsets.only(bottom: 8.h),
+        padding: EdgeInsets.only(top: 20.h,bottom: 24.w,left: 16.w),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(16.r),
+        ),
+      
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Note Title',
                 style: TextStyle(
-                  color: Colors.black.withValues(alpha: 0.5),
-                  fontSize: 18.sp,
+                  color: Colors.black,
+                  fontSize: 28.sp,
+                ),
+              ),
+      
+              subtitle: Padding(
+                padding:  EdgeInsets.symmetric(vertical: 16.h),
+                child: Text(
+                  'Note content goes here...',
+                  style: TextStyle(
+                    color: Colors.black.withValues(alpha: 0.5),
+                    fontSize: 18.sp,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 30.sp,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 30.sp,
+            Padding(
+              padding:  EdgeInsets.only(right: 16.w),
+              child: Text(
+                'Created on: 2024-06-01',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16.sp,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding:  EdgeInsets.only(right: 16.w),
-            child: Text(
-              'Created on: 2024-06-01',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 16.sp,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
