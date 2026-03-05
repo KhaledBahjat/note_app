500289e51f1a4032406dc991ce942c13b412219f
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-03-05 12:52:33 +0200

    feat: create NoteModel class to represent note data structure

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title;
+  final String content;
+  final int colorr;
+  final String date;
+  NoteModel({
+    required this.title,
+    required this.content,
+    required this.colorr,
+    required this.date,
+  });
+}
