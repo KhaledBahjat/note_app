a87224d60b3831e2d1ec49baa80d6788df77302e
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-03-06 15:24:07 +0200

    feat: integrate flutter_bloc and add AddNotesCubit to main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/cubits/add_notes_cubit/add_notes_cubit.dart';
 import 'package:note_app/helper/constant.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/notes_view.dart';
@@ -20,10 +22,15 @@ class NotesApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(370, 690),
       builder: (context, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-          home: const NotesView(),
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (context) => AddNotesCubit(),)
+          ],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+            home: const NotesView(),
+          ),
         );
       },
     );
