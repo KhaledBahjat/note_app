28c0e28dd0d6d0ff3a3bb7098e06934add23e52e
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-03-06 15:32:17 +0200

    feat: refactor AddNoteBottomSheet to use BlocConsumer for state management and loading indicators

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/add_notes_cubit/add_notes_cubit.dart';
 import 'package:note_app/helper/spacing.dart';
 import 'package:note_app/views/widgets/custom_button.dart';
 import 'package:note_app/views/widgets/custom_textfeild.dart';
@@ -33,53 +36,74 @@ class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
         bottom: MediaQuery.of(context).viewInsets.bottom + 20.h,
       ),
       child: SingleChildScrollView(
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Add Note',
-                style: TextStyle(
-                  fontSize: 24.sp,
-                  fontWeight: FontWeight.bold,
+        child: BlocConsumer<AddNotesCubit, AddNotesState>(
+          listener: (context, state) {
+            if (state is AddNotesSuccess) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Note added successfully')),
+              );
+              Navigator.pop(context);
+            }
+              else if (state is AddNotesFailure) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text(state.errorMessage)),
+                );
+              }
+              
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNotesLoading,
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Add Note',
+                      style: TextStyle(
+                        fontSize: 24.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    HeightSpace(height: 24),
+                    CustomTextField(
+                      controller: titleController,
+                      hintText: 'Title',
+                      maxLines: 1,
+                      onSaved: (value) {
+                        titleController.text = value ?? '';
+                      },
+                    ),
+                    HeightSpace(height: 16),
+                    CustomTextField(
+                      controller: contentController,
+                      hintText: 'Content',
+                      maxLines: 5,
+                      onSaved: (value) {
+                        contentController.text = value ?? '';
+                      },
+                    ),
+                    HeightSpace(height: 24),
+                    CustomButton(
+                      onTap: () {
+                        if (formKey.currentState!.validate()) {
+                          formKey.currentState!.save();
+
+                          Navigator.pop(context);
+                        } else {
+                          setState(() {
+                            autovalidateMode = AutovalidateMode.always;
+                          });
+                        }
+                      },
+                    ),
+                    HeightSpace(height: 10),
+                  ],
                 ),
               ),
-              HeightSpace(height: 24),
-              CustomTextField(
-                controller: titleController,
-                hintText: 'Title',
-                maxLines: 1,
-                onSaved: (value) {
-                  titleController.text = value ?? '';
-                },
-              ),
-              HeightSpace(height: 16),
-              CustomTextField(
-                controller: contentController,
-                hintText: 'Content',
-                maxLines: 5,
-                onSaved: (value) {
-                  contentController.text = value ?? '';
-                },
-              ),
-              HeightSpace(height: 24),
-              CustomButton(
-                onTap: () {
-                  if (formKey.currentState!.validate()) {
-                    formKey.currentState!.save();
-
-                    Navigator.pop(context);
-                  } else {
-                    setState(() {
-                      autovalidateMode = AutovalidateMode.always;
-                    });
-                  }
-                },
-              ),
-              HeightSpace(height: 10),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
